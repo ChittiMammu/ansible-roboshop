c8076346f0fafaf64e68ebaee1c3d0a83560b2b7
ChittiMammu <mammu.malluru@gmail.com>
2024-06-17 19:08:22 +0530

    mongodb-yaml 3

--- ansible-roboshop/roboshell.sh
+++ ansible-roboshop/roboshell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI=ami-0b4f379183e5706b9 #this keeps on changing
-SG_ID=sg-0e1d98e695074225a #replace with your SG ID
+SG_ID=sg-0ec6c8f5a0dc7d587 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z05281582RQ01LB1JNU7W # replace your zone ID
 DOMAIN_NAME="malluru.online"
@@ -16,7 +16,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-0e1d98e695074225a --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-0ec6c8f5a0dc7d587 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
